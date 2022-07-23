911c39d0b07e7af794d29668040b3e8d41c8577f
Stefano Pelagotti <pelagotti.s@gmail.com>
2022-07-23 18:40:41 +0200

    Added rate limiting to ApiController
    Now the controller can throttle request based on method and the configured parameters.
    The two methods do the same thing, and could be merged into one, but for the sake of the POC (which assumes a more
    complex app) I kept the separated.

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Interfaces\RateLimiterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,7 +12,24 @@ class ApiController extends AbstractController {
     public const OK_STATUS_CODE = 200;
     public const TOO_MANY_REQUESTS_STATUS_CODE = 429;
 
+    public function __construct(
+        private RateLimiterInterface $rateLimiter,
+    ) {
+
+    }
+
     public function handleGet(Request $request): Response {
+        if ($this->rateLimiter->requestShouldBeLimited($request)) {
+            return new JsonResponse([
+                "status" => 'ko',
+                "message" => $this->rateLimiter->getMessage(),
+            ],
+                self::TOO_MANY_REQUESTS_STATUS_CODE,
+                [
+                    'Retry-After' => $this->rateLimiter->getRetryAfter(),
+                ]
+            );
+        }
         return new JsonResponse([
             "status" => 'ok',
             "message" => 'Hello GET request! You can also PoST at the same endpoint!',
@@ -20,10 +38,21 @@ class ApiController extends AbstractController {
     }
 
     public function handlePost(Request $request): Response {
+        if ($this->rateLimiter->requestShouldBeLimited($request)) {
+            return new JsonResponse([
+                "status" => 'ko',
+                "message" => $this->rateLimiter->getMessage(),
+            ],
+                self::TOO_MANY_REQUESTS_STATUS_CODE,
+                [
+                    'Retry-After' => $this->rateLimiter->getRetryAfter(),
+                ]
+            );
+        }
         return new JsonResponse([
             "status" => 'ok',
             "message" => 'See? I told you it would work!!',
         ],
-            self::OK_STATUS_CODE);
+            self::OK_STATUS_CODE,);
     }
 }
